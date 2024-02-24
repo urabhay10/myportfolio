18f6e0b680a6b2c25b166531cb997d9fe136c226
Abhay Upadhyay <urabhay10@gmail.com>
2024-02-24 16:56:46 +0530

    deploying

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div style={{
-        overflow: window.innerWidth>=window.innerHeight && window.innerWidth>768 ? 'hidden' : 'scroll'
+        overflow: (window.innerWidth>=window.innerHeight && window.innerWidth>768) ? 'hidden' : 'scroll'
       }}>
         {window.innerWidth>=window.innerHeight && window.innerWidth>768 && <DesktopVersion/>}
         {!(window.innerWidth>=window.innerHeight && window.innerWidth>768) && <MobileVersion/>}
